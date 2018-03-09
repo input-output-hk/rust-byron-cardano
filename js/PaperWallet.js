7fd7f0518a76b2f8e5e9d96b73a1a529284c709a
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-03-09 15:07:23 +0000

    use the right variable for unscramble

--- js/PaperWallet.js
+++ js/PaperWallet.js
@@ -27,7 +27,7 @@ export const unscramble = (module, password, input) => {
   const bufpassword = newArray(module, password);
   const bufoutput = newArray0(module, input.length - 4);
   module.paper_unscramble(bufpassword, password.length, bufinput, input.length, bufoutput);
-  let result = copyArray(module, bufxprv, input.length - 4);
+  let result = copyArray(module, bufoutput, input.length - 4);
   module.dealloc(bufinput);
   module.dealloc(bufpassword);
   module.dealloc(bufoutput);
