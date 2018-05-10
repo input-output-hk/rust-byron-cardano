30139bacf7eaa624bce7c69f6c7c16c0ab5bb251
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-05-11 00:55:25 +0100

    add util to generate hash

--- storage/src/types.rs
+++ storage/src/types.rs
@@ -1,7 +1,15 @@
+use blockchain::HeaderHash;
+
 pub const HASH_SIZE : usize = 32;
 
 pub type BlockHash = [u8;HASH_SIZE];
 pub type PackHash = [u8;HASH_SIZE];
+
+pub fn header_to_blockhash(header_hash: &HeaderHash) -> BlockHash {
+    let mut bh = [0u8;HASH_SIZE];
+    bh[0..HASH_SIZE].clone_from_slice(header_hash.as_ref());
+    bh
+}
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub enum StorageFileType {
