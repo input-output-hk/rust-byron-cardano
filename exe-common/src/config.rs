c7ebe86a6a8048bd3b28801f83a5c9fb6c459731
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-05-20 22:40:25 +0100

    add proper parameters for both nets

--- exe-common/src/config.rs
+++ exe-common/src/config.rs
@@ -8,6 +8,7 @@ pub mod net {
     pub struct Config {
         pub domain: String,
         pub genesis: HeaderHash,
+        pub genesis_prev: HeaderHash,
         pub protocol_magic: ProtocolMagic,
         pub epoch_start: EpochId,
     }
@@ -16,6 +17,7 @@ pub mod net {
             Config {
                 domain: "relays.cardano-mainnet.iohk.io:3000".to_string(),
                 genesis: HeaderHash::from_hex(&"89D9B5A5B8DDC8D7E5A6795E9774D97FAF1EFEA59B2CAF7EAF9F8C5B32059DF4").unwrap(),
+                genesis_prev: HeaderHash::from_hex(&"5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb").unwrap(),
                 protocol_magic: ProtocolMagic::default(),
                 epoch_start: 0,
             }
@@ -24,10 +26,10 @@ pub mod net {
         pub fn testnet() -> Self {
             Config {
                 domain: "relays.awstest.iohkdev.io:3000".to_string(),
-                //genesis: HeaderHash::from_hex(&"B365F1BE6863B453F12B93E1810909B10C79A95EE44BF53414888513FE172C90").unwrap(),
-                genesis: HeaderHash::from_hex(&"fcccb905dac43709c987d9b15de62de89bbee2770a0f21ff1f0b57b84a6012b8").unwrap(),
+                genesis: HeaderHash::from_hex(&"B365F1BE6863B453F12B93E1810909B10C79A95EE44BF53414888513FE172C90").unwrap(),
+                genesis_prev: HeaderHash::from_hex(&"c6a004d3d178f600cd8caa10abbebe1549bef878f0665aea2903472d5abf7323").unwrap(),
                 protocol_magic: ProtocolMagic::new(633343913),
-                epoch_start: 4,
+                epoch_start: 0,
             }
         }
 
