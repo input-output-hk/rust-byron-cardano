c203e44e92a7c4cdbf06b69f8fd63694fa2720f4
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-05-20 22:43:38 +0100

    adjust offsets for searching in offsets

--- storage/src/pack.rs
+++ storage/src/pack.rs
@@ -37,12 +37,12 @@ const FANOUT_OFFSET : usize = MAGIC_SIZE + 8;
 const BLOOM_OFFSET : usize = FANOUT_OFFSET + FANOUT_SIZE;
 
 // calculate the file offset from where the hashes are stored
-fn offset_hashes(bloom_size: usize) -> u64 {
+fn offset_hashes(bloom_size: u32) -> u64 {
     8 + 8 + FANOUT_SIZE as u64 + bloom_size as u64
 }
 
 // calculate the file offset from where the offsets are stored
-fn offset_offsets(bloom_size: usize, number_hashes: u32) -> u64 {
+fn offset_offsets(bloom_size: u32, number_hashes: u32) -> u64 {
     offset_hashes(bloom_size) + HASH_SIZE as u64 * number_hashes as u64
 }
 
@@ -50,7 +50,14 @@ type Offset = u64;
 type Size = u32;
 pub type IndexOffset = u32;
 
+// The parameters associated with the index file.
+// * the bloom filter size in bytes
+pub struct Params {
+    pub bloom_size: u32,
+}
+
 pub struct Lookup {
+    pub params: Params,
     pub fanout: Fanout,
     pub bloom: Bloom,
 }
@@ -58,6 +65,7 @@ pub struct Lookup {
 pub struct Fanout([u32;FANOUT_ELEMENTS]);
 pub struct FanoutStart(u32);
 pub struct FanoutNb(pub u32);
+pub struct FanoutTotal(u32);
 
 impl Fanout {
     pub fn get_indexer_by_hash(&self, hash: &super::BlockHash) -> (FanoutStart, FanoutNb) {
@@ -73,8 +81,8 @@ impl Fanout {
             },
         }
     }
-    pub fn get_total(&self) -> FanoutNb {
-        FanoutNb(self.0[255])
+    pub fn get_total(&self) -> FanoutTotal {
+        FanoutTotal(self.0[255])
     }
 }
 
@@ -136,6 +144,20 @@ fn file_read_hash(mut file: &fs::File) -> super::BlockHash {
     buf
 }
 
+// the default size (in bytes) of the bloom filter related to the number of
+// expected entries in the files.
+pub fn default_bloom_size(entries: usize) -> u32 {
+    if entries < 0x1000 {
+        4096
+    } else if entries < 0x5000 {
+        8192
+    } else if entries < 0x22000 {
+        16384
+    } else {
+        32768
+    }
+}
+
 pub fn create_index(storage: &super::Storage, index: &Index) -> (Lookup, super::TmpFile) {
     let mut tmpfile = super::tmpfile_create_type(storage, super::StorageFileType::Index);
     let mut hdr_buf = [0u8;HEADER_SIZE];
@@ -144,18 +166,11 @@ pub fn create_index(storage: &super::Storage, index: &Index) -> (Lookup, super::
 
     assert!(entries == index.offsets.len());
 
-    let bloom_size = if entries < 0x1000 {
-        4096
-    } else if entries < 0x5000 {
-        8192
-    } else if entries < 0x22000 {
-        16384
-    } else {
-        32768
-    };
+    let bloom_size = default_bloom_size(entries);
+    let params = Params { bloom_size: bloom_size };
 
     hdr_buf[0..8].clone_from_slice(&MAGIC[..]);
-    write_size(&mut hdr_buf[8..12], bloom_size);
+    write_size(&mut hdr_buf[8..12], bloom_size as u32);
     write_size(&mut hdr_buf[12..16], 0);
 
     // write fanout to hdr_buf
@@ -202,7 +217,7 @@ pub fn create_index(storage: &super::Storage, index: &Index) -> (Lookup, super::
         write_offset(&mut buf, ofs);
         tmpfile.write_all(&buf[..]).unwrap();
     }
-    (Lookup { fanout: fanout, bloom: Bloom(bloom) }, tmpfile)
+    (Lookup { params: params, fanout: fanout, bloom: Bloom(bloom) }, tmpfile)
 }
 
 pub fn open_index(storage_config: &super::StorageConfig, pack: &super::PackHash) -> fs::File {
@@ -214,7 +229,7 @@ pub fn dump_index(storage_config: &super::StorageConfig, pack: &super::PackHash)
     let lookup = index_get_header(&mut file)?;
 
     let mut v = Vec::new();
-    let FanoutNb(total) = lookup.fanout.get_total();
+    let FanoutTotal(total) = lookup.fanout.get_total();
 
     file.seek(SeekFrom::Start(HEADER_SIZE as u64)).unwrap();
     for _ in 0..total {
@@ -242,7 +257,11 @@ pub fn index_get_header(mut file: &fs::File) -> io::Result<Lookup> {
 
     file.read_exact(&mut bloom[..])?;
 
-    Ok(Lookup { fanout: Fanout(fanout), bloom: Bloom(bloom) })
+    Ok(Lookup {
+        params: Params { bloom_size: bloom_size },
+        fanout: Fanout(fanout),
+        bloom: Bloom(bloom)
+    })
 }
 
 pub fn read_index_fanout(storage_config: &super::StorageConfig, pack: &super::PackHash) -> io::Result<Lookup> {
@@ -253,8 +272,8 @@ pub fn read_index_fanout(storage_config: &super::StorageConfig, pack: &super::Pa
 // conduct a search in the index file, returning the offset index of a found element
 //
 // TODO switch to bilinear search with n > something
-pub fn search_index(mut file: &fs::File, bloom_size: usize, blk: &super::BlockHash, start_elements: FanoutStart, hier_elements: FanoutNb) -> Option<IndexOffset> {
-    let hsz = offset_hashes(bloom_size);
+pub fn search_index(mut file: &fs::File, params: &Params, blk: &super::BlockHash, start_elements: FanoutStart, hier_elements: FanoutNb) -> Option<IndexOffset> {
+    let hsz = offset_hashes(params.bloom_size);
     match hier_elements.0 {
         0 => None,
         1 => {
@@ -291,8 +310,9 @@ pub fn search_index(mut file: &fs::File, bloom_size: usize, blk: &super::BlockHa
 }
 
 pub fn resolve_index_offset(mut file: &fs::File, lookup: &Lookup, index_offset: IndexOffset) -> Offset {
-    let FanoutNb(total) = lookup.fanout.get_total();
-    let ofs = HEADER_SIZE as u64 + HASH_SIZE as u64 * total as u64 + OFF_SIZE as u64 * index_offset as u64;
+    let FanoutTotal(total) = lookup.fanout.get_total();
+    let ofs_base = offset_offsets(lookup.params.bloom_size, total);
+    let ofs = ofs_base + OFF_SIZE as u64 * index_offset as u64;
     file.seek(SeekFrom::Start(ofs)).unwrap();
     file_read_offset(&mut file)
 }
