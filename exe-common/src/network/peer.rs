8a91234aae128ded02d971d65128d54d7fd722a0
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-05-25 11:28:43 +0100

    more Api impl

--- exe-common/src/network/peer.rs
+++ exe-common/src/network/peer.rs
@@ -1,6 +1,8 @@
 use config;
-use network::{native, Error, Result};
+use network::{native, Result, api::{*}};
 use wallet_crypto::config::{ProtocolMagic};
+use blockchain::{BlockHeader, Block, HeaderHash};
+use storage::{Storage};
 
 /// network object to handle a peer connection and redirect to constructing
 /// the appropriate network protocol object (native, http...)
@@ -16,6 +18,25 @@ impl Peer {
             config::net::Peer::Http(addr) => {
                 unimplemented!("connot to connect to peer (`{}') address `{}'", name, addr);
             }
+        }
+    }
+}
+impl Api for Peer {
+    fn get_tip(&mut self) -> Result<BlockHeader> {
+        match self {
+            Peer::Native(peer) => peer.get_tip(),
+        }
+    }
+
+    fn get_block(&mut self, hash: HeaderHash) -> Result<Block> {
+        match self {
+            Peer::Native(peer) => peer.get_block(hash),
+        }
+    }
+
+    fn fetch_epoch(&mut self, storage: &mut Storage, fep: FetchEpochParams) -> Result<FetchEpochResult> {
+        match self {
+            Peer::Native(peer) => peer.fetch_epoch(storage, fep),
         }
     }
 }
