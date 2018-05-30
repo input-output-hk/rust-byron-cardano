e0c5ddd634ce46991e55fa4c38b23f6029a42c39
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-05-30 16:07:33 +0100

    remove the need to clone the address

--- wallet-cli/src/command/wallet/state.rs
+++ wallet-cli/src/command/wallet/state.rs
@@ -111,34 +111,33 @@ impl RandomIndexLookup {
     pub fn new(root_pk: &hdwallet::XPub) -> Result<Self> {
         Ok(RandomIndexLookup { key: hdpayload::HDKey::new(root_pk) })
     }
+
+    fn one_of_mine(&self, addr: &ExtendedAddr) -> bool {
+        match addr.attributes.derivation_path {
+            None => false,
+            Some(ref epath) => {
+                match self.key.decrypt_path(epath) {
+                    None => false,
+                    Some(ref _path) => {
+                        // TODO verify that the address really belongs to us
+                        // by deriving the private key using the path
+                        true
+                    },
+                }
+            },
+        }
+    }
 }
 
 impl AddrLookup for RandomIndexLookup {
     fn lookup(&mut self, outs: &[&TxOut]) -> Result<Vec<TxOut>> {
         let mut found = Vec::new();
         for o in outs {
-            if is_our_ri_address(&self.key, &o.address.clone()) {
+            if self.one_of_mine(&o.address) {
                 found.push(o.clone().clone())
             }
         }
         Ok(found)
-    }
-}
-
-// check if an address is an old style random index address
-fn is_our_ri_address(key: &hdpayload::HDKey, addr: &ExtendedAddr) -> bool {
-    match addr.attributes.derivation_path {
-        None => false,
-        Some(ref epath) => {
-            match key.decrypt_path(epath) {
-                None => false,
-                Some(ref _path) => {
-                    // TODO verify that the address really belongs to us
-                    // by deriving the private key using the path
-                    true
-                },
-            }
-        },
     }
 }
 
