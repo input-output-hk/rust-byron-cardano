c57971cc695eacd5349a9ba719838b6ec8fc3c5f
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-06-01 10:15:56 +0100

    prevent adding empty entries in the append file

--- storage/src/append.rs
+++ storage/src/append.rs
@@ -74,6 +74,8 @@ impl Writer {
     /// The slice **must** contain all the bytes that needs to be written in the
     /// append only file.
     pub fn append_bytes(&mut self, bytes: &[u8]) -> Result<()> {
+        if bytes.is_empty() { return Ok(()) }
+
         let len = bytes.len() as Size;
         let mut sz_buf = [0u8;SIZE_SIZE];
         write_size(&mut sz_buf, len);
