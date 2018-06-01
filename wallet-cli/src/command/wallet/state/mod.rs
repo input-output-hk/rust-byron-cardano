3422d52dc113f59b5dedb4b967b16bc637875c30
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-06-01 08:57:56 +0100

    update the command wallet update skeleton

--- wallet-cli/src/command/wallet/state/mod.rs
+++ wallet-cli/src/command/wallet/state/mod.rs
@@ -42,12 +42,38 @@ impl HasCommand for Update {
         //
         // let network_tip = storage.get_tip().unwrap();
 
-        // 3. perform the lookup now. We may want to save wallet logs as we find them:
+        // 3. from the wallet_state we need to be able to get the starting point
+        //    or actually, the last known state_ptr (BlockDate and Hash);
+        //
+        // let current_ptr = wallet_state.state_prt()
+
+        // 4. wallet current utxos
+        //    we need to be able to retrieve them from the wallet
+        //
+        // let current_utxos = wallet_state.utxos().clone();
+        let current_utxos = lookup::Utxos::new();
+
+        // 5. we need to be able to retrieve the wallet's lookup structure
+        //
+        // i.e. we need to know if it is a bip44 or a random address method
+        //      for now we assume a bip44 sequential indexing
+        let lookup_structure = sequentialindex::SequentialBip44Lookup::new(wallet_cfg.wallet().unwrap());
+
+        // 6. construct the lookup state from the current_ptr
+        // let mut state = lookup::State::new(current_ptr, current_utxos, lookup_structure);
+
+        // 7. perform the lookup now. We may want to save wallet logs as we find them:
         //    - so we know when to start from again (so we need checkpoint or something similar)
         //    - ... non exhausted list of element to log, we will need to look at utxo and co
         //
         //    we also need to update the wallet state on the fly so
         //    we can display something to the user too
+
+        let mut iter = storage.iterate_from_epoch(0).unwrap();
+        while let Some(blk) = iter.next_block().unwrap() {
+            // state.forward(&[blk]).unwrap();
+        }
+
         unimplemented!()
     }
 }
