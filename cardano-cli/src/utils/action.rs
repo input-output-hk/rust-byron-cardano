663340122aa8221184e863975f38d3e4950a2e13
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-07-19 12:59:15 +0100

    update the documentation relating to the handling of the sequential tasks

--- cardano-cli/src/utils/action.rs
+++ cardano-cli/src/utils/action.rs
@@ -47,15 +47,22 @@
 use std::{time::{Duration}, thread, sync::{Arc, Mutex, Condvar}, mem};
 use super::super::utils::term::{Progress};
 
+/// represent an asynchronous result, either the result is Ready or
+/// it is not ready.
+///
+/// This is very similar to `Option<T>` but it has a special meaning.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
 pub enum Async<T> {
+    /// the asynchronous operation finished and here is the result.
     Ready(T),
+    /// the asynchronous operation is not finished yet.
     NotReady
 }
 unsafe impl<T: Send> Send for Async<T> {}
 unsafe impl<T: Sync> Sync for Async<T> {}
 
 impl<T> Async<T> {
+    /// map the result of the async operation
     pub fn map<F, U>(self, f: F) -> Async<U>
         where F: FnOnce(T) -> U
     {
@@ -64,6 +71,8 @@ impl<T> Async<T> {
             Async::Ready(t) => Async::Ready(f(t))
         }
     }
+
+    /// tell if the Async is ready to unwrap or not
     pub fn ready(&self) -> bool {
         match self {
             Async::NotReady => false,
@@ -77,9 +86,93 @@ pub enum Error {
 
 }
 
+/// convenient alias for polling the result of a given operation.
 pub type Poll<T> = Result<Async<T>, Error>;
 
-/// a crate a is
+/// sequential operation manager
+///
+/// Will hold references to the running tasks. They will be executed in
+/// the same sequential order as the call to `spawn` function.
+///
+/// See module documentation for an example.
+///
+pub struct Executor<T> {
+    all_threads: Vec<thread::JoinHandle<()>>,
+    current_receiver: Crate<T>,
+    current_worker: Crate<String>
+
+}
+impl<T: Send + 'static> Executor<T> {
+
+    /// create a new executor manager with the initial value,
+    /// the first spawning task will consume this initial value
+    /// and will start straight away.
+    ///
+    pub fn new(initial: T) -> Self {
+        let r = Crate::new();
+        r.store(initial).unwrap();
+
+        Executor {
+            all_threads: Vec::with_capacity(10),
+            current_receiver: r,
+            current_worker: Crate::new()
+        }
+    }
+
+    /// spawn a new task. this function append the given task to the sequence of
+    /// functions to execute. Consuming the return value of the previously spawn
+    /// task (or the initial value is this is the first call to spawn).
+    ///
+    pub fn spawn<F, Q>(self, alias: &'static str, f: F) -> Executor<Q>
+        where F: FnOnce(T) -> Q
+            , F: Send + 'static
+            , Q: Send + 'static
+    {
+        let input = self.current_receiver.clone();
+        let mut all_threads = self.all_threads;
+        let output = Crate::new();
+        let current_receiver = output.clone();
+        let current_worker = self.current_worker.clone();
+        let thread = thread::spawn(move || {
+            let res = loop {
+                match input.await_result().unwrap() {
+                    Async::NotReady => {},
+                    Async::Ready(v) => break(v)
+                }
+            };
+            current_worker.store(alias.to_owned()).unwrap();
+            output.store(f(res)).unwrap();
+        });
+
+        all_threads.push(thread);
+        Executor {
+            all_threads,
+            current_receiver,
+            current_worker: self.current_worker
+        }
+    }
+
+    /// pause the calling thread until the `store` function is called.
+    ///
+    /// This function will also update the given `ticker` with the currently
+    /// working task (see alias parameter of the spawn function) and the
+    /// general progress.
+    pub fn poll<'a>(&self, ticker: &mut Progress<'a>) -> Poll<T> {
+        loop {
+            if let Some(r) = self.current_receiver.await_result_timeout(Duration::from_millis(100)).unwrap() {
+                return Ok(r);
+            } else {
+                if let Some(Async::Ready(worker_alias)) = self.current_worker.await_result_timeout(Duration::new(0, 0)).unwrap() {
+                    ticker.message(&worker_alias);
+                    ticker.advance(1);
+                } else {
+                    ticker.tick();
+                }
+            }
+        }
+    }
+}
+
 struct Crate<T>(Arc<(Mutex<Async<T>>, Condvar)>);
 impl<T> Crate<T> {
     fn new() -> Self {
@@ -137,69 +230,5 @@ impl<T> Crate<T> {
         mem::swap(&mut *result, &mut val);
 
         Ok(Some(val))
-    }
-}
-
-pub struct Executor<T> {
-    all_threads: Vec<thread::JoinHandle<()>>,
-    current_receiver: Crate<T>,
-    current_worker: Crate<String>
-
-}
-impl<T: Send + 'static> Executor<T> {
-    pub fn new(initial: T) -> Self {
-        let r = Crate::new();
-        r.store(initial).unwrap();
-
-        Executor {
-            all_threads: Vec::with_capacity(10),
-            current_receiver: r,
-            current_worker: Crate::new()
-        }
-    }
-
-    pub fn spawn<F, Q>(self, alias: &'static str, f: F) -> Executor<Q>
-        where F: FnOnce(T) -> Q
-            , F: Send + 'static
-            , Q: Send + 'static
-    {
-        let input = self.current_receiver.clone();
-        let mut all_threads = self.all_threads;
-        let output = Crate::new();
-        let current_receiver = output.clone();
-        let current_worker = self.current_worker.clone();
-        let thread = thread::spawn(move || {
-            let res = loop {
-                match input.await_result().unwrap() {
-                    Async::NotReady => {},
-                    Async::Ready(v) => break(v)
-                }
-            };
-            current_worker.store(alias.to_owned()).unwrap();
-            output.store(f(res)).unwrap();
-        });
-
-        all_threads.push(thread);
-        Executor {
-            all_threads,
-            current_receiver,
-            current_worker: self.current_worker
-        }
-    }
-
-    /// pause the calling thread until the `store` function is called.
-    pub fn poll<'a>(&self, ticker: &mut Progress<'a>) -> Poll<T> {
-        loop {
-            if let Some(r) = self.current_receiver.await_result_timeout(Duration::from_millis(100)).unwrap() {
-                return Ok(r);
-            } else {
-                if let Some(Async::Ready(worker_alias)) = self.current_worker.await_result_timeout(Duration::new(0, 0)).unwrap() {
-                    ticker.message(&worker_alias);
-                    ticker.advance(1);
-                } else {
-                    ticker.tick();
-                }
-            }
-        }
     }
 }
