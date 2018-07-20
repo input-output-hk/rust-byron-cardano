e625d255d904f2adaaf394608646d8a07d138512
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-07-20 14:11:06 +0100

    add the key generation

---
+++ cardano/src/wallet/keygen.rs
@@ -0,0 +1,17 @@
+use bip::bip39;
+
+use cryptoxide::hmac::{Hmac};
+use cryptoxide::sha2::{Sha512};
+use cryptoxide::pbkdf2::{pbkdf2};
+
+pub type Entropy = bip39::Entropy;
+
+/// Create a new seed from entropy and password
+/// 
+/// The output size of pbkdf2 is associated with the size of the slice, allowing
+/// to generate a seed of the size required for various specific cryptographic object
+pub fn generate_seed(entropy: &Entropy, password: &[u8], output: &mut [u8]) {
+    const ITER : u32 = 4096;
+    let mut mac = Hmac::new(Sha512::new(), password);
+    pbkdf2(&mut mac, entropy.as_ref(), ITER, output)
+}
--- cardano/src/wallet/mod.rs
+++ cardano/src/wallet/mod.rs
@@ -1,3 +1,4 @@
 pub mod bip44;
 pub mod rindex;
 pub mod scheme;
+pub mod keygen;
