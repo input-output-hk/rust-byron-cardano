ef5f0705bcd918834e2bd901f41cbcb5db3b9643
Eelco Dolstra <edolstra@gmail.com>
2018-07-27 16:41:54 +0200

    Rename k -> epoch_stability_depth

--- exe-common/src/config.rs
+++ exe-common/src/config.rs
@@ -192,7 +192,7 @@ pub mod net {
     pub struct Config {
         pub genesis: HeaderHash,
         pub genesis_prev: HeaderHash,
-        pub k: u32,
+        pub epoch_stability_depth: u32,
         pub protocol_magic: ProtocolMagic,
         pub epoch_start: EpochId,
         pub peers: Peers
@@ -205,7 +205,7 @@ pub mod net {
             Config {
                 genesis: HeaderHash::from_hex(&"89D9B5A5B8DDC8D7E5A6795E9774D97FAF1EFEA59B2CAF7EAF9F8C5B32059DF4").unwrap(),
                 genesis_prev: HeaderHash::from_hex(&"5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb").unwrap(),
-                k: 2160,
+                epoch_stability_depth: 2160,
                 protocol_magic: ProtocolMagic::default(),
                 epoch_start: 0,
                 peers: peers
@@ -219,7 +219,7 @@ pub mod net {
             Config {
                 genesis: HeaderHash::from_hex(&"B365F1BE6863B453F12B93E1810909B10C79A95EE44BF53414888513FE172C90").unwrap(),
                 genesis_prev: HeaderHash::from_hex(&"c6a004d3d178f600cd8caa10abbebe1549bef878f0665aea2903472d5abf7323").unwrap(),
-                k: 2160,
+                epoch_stability_depth: 2160,
                 protocol_magic: ProtocolMagic::new(633343913),
                 epoch_start: 0,
                 peers: peers
