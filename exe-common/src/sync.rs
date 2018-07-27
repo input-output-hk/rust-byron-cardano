838719486cb8c446f8d96e119ed934f64304868b
Eelco Dolstra <edolstra@gmail.com>
2018-07-27 10:30:53 +0200

    net_sync(): Pack the epoch prior to the current local tip if necessary

--- exe-common/src/sync.rs
+++ exe-common/src/sync.rs
@@ -73,26 +73,16 @@ pub fn net_sync(net: &mut Api, net_cfg: &net::Config, storage: storage::Storage)
         let mut writer = storage::pack::PackWriter::init(&storage.config);
         let epoch_time_start = SystemTime::now();
 
-        let mut cur_hash = our_tip.0.hash.clone();
-        let mut blocks = vec![];
-        loop {
-            let block_raw = block_read(&storage, cur_hash.bytes()).unwrap();
-            let block = block_raw.decode().unwrap();
-            let hdr = block.get_header();
-            assert!(hdr.get_blockdate().get_epochid() == epoch_id);
-            blocks.push((storage::types::header_to_blockhash(&cur_hash), block_raw));
-            if hdr.get_blockdate().is_genesis() { break }
-            cur_hash = hdr.get_previous_header();
-        }
-
-        while let Some((hash, block_raw)) = blocks.pop() {
-            writer.append(&hash, block_raw.as_ref());
-        }
+        let prev_block = append_blocks_to_epoch_reverse(
+            &storage, epoch_id, &mut writer, &our_tip.0.hash);
 
         cur_epoch_state = Some((epoch_id, writer, epoch_time_start));
 
-        // FIXME: in fact we may need to pack the previous epoch as
-        // well. (If tip.slotid < w, it won't have been packed yet).
+        // If tip.slotid < w, the previous epoch won't have been
+        // created yet either, so do that now.
+        if epoch_id > net_cfg.epoch_start {
+            maybe_create_epoch(&storage, epoch_id - 1, &prev_block);
+        }
     }
 
     net.get_blocks(&our_tip.0, our_tip.1, &tip, &mut |block_hash, block, block_raw| {
@@ -101,21 +91,11 @@ pub fn net_sync(net: &mut Api, net_cfg: &net::Config, storage: storage::Storage)
         // Flush the previous epoch (if any).
         if date.is_genesis() {
             if let Some((epoch_id, writer, epoch_time_start)) = cur_epoch_state.as_mut() {
-                let (packhash, index) = writer.finalize();
-                let (_, tmpfile) = storage::pack::create_index(&storage, &index);
-                tmpfile.render_permanent(&storage.config.get_index_filepath(&packhash)).unwrap();
-                let epoch_time_elapsed = epoch_time_start.elapsed().unwrap();
-
-                storage::tag::write(&storage, &storage::tag::get_epoch_tag(*epoch_id), &packhash[..]);
-
-                storage::epoch::epoch_create(&storage.config, &packhash, *epoch_id);
+                finish_epoch(&storage, *epoch_id, writer, epoch_time_start);
 
                 // Checkpoint the tip so we don't have to refetch
                 // everything if we get interrupted.
                 storage::tag::write(&storage, &tag::HEAD, &last_block.as_ref().unwrap().bytes()[..]);
-
-                info!("=> pack {} written for epoch {} in {}", hex::encode(&packhash[..]),
-                      epoch_id, duration_print(epoch_time_elapsed));
             }
         }
 
@@ -145,6 +125,69 @@ pub fn net_sync(net: &mut Api, net_cfg: &net::Config, storage: storage::Storage)
         storage::tag::write(&storage, &tag::HEAD,
                             &storage::types::header_to_blockhash(&block_hash));
     }
+}
+
+// Create an epoch from a complete set of previously fetched blocks on
+// disk.
+fn maybe_create_epoch(storage: &storage::Storage, epoch_id: EpochId, last_block: &HeaderHash)
+{
+    // FIXME: epoch_read() is a bit inefficient here; we really only
+    // want to know if it exists.
+    if storage::epoch::epoch_read(&storage.config, epoch_id).is_ok() { return }
+
+    info!("Packing epoch {}", epoch_id);
+
+    let mut writer = storage::pack::PackWriter::init(&storage.config);
+    let epoch_time_start = SystemTime::now();
+
+    append_blocks_to_epoch_reverse(&storage, epoch_id, &mut writer, last_block);
+
+    finish_epoch(storage, epoch_id, &mut writer, &epoch_time_start);
+
+    // TODO: delete the blocks from disk?
+}
+
+fn append_blocks_to_epoch_reverse(
+    storage: &storage::Storage,
+    epoch_id : EpochId,
+    writer : &mut storage::pack::PackWriter,
+    last_block: &HeaderHash)
+    -> HeaderHash
+{
+    let mut cur_hash = last_block.clone();
+    let mut blocks = vec![];
+    loop {
+        let block_raw = block_read(&storage, cur_hash.bytes()).unwrap();
+        let block = block_raw.decode().unwrap();
+        let hdr = block.get_header();
+        assert!(hdr.get_blockdate().get_epochid() == epoch_id);
+        blocks.push((storage::types::header_to_blockhash(&cur_hash), block_raw));
+        cur_hash = hdr.get_previous_header();
+        if hdr.get_blockdate().is_genesis() { break }
+    }
+
+    while let Some((hash, block_raw)) = blocks.pop() {
+        writer.append(&hash, block_raw.as_ref());
+    }
+
+    cur_hash
+}
+
+fn finish_epoch(storage: &storage::Storage, epoch_id : EpochId, writer : &mut storage::pack::PackWriter, epoch_time_start : &SystemTime)
+{
+    let (packhash, index) = writer.finalize();
+    let (_, tmpfile) = storage::pack::create_index(&storage, &index);
+    tmpfile.render_permanent(&storage.config.get_index_filepath(&packhash)).unwrap();
+    let epoch_time_elapsed = epoch_time_start.elapsed().unwrap();
+
+    // TODO: should test that epoch <epoch_id - 1> exists.
+
+    storage::tag::write(&storage, &storage::tag::get_epoch_tag(epoch_id), &packhash[..]);
+
+    storage::epoch::epoch_create(&storage.config, &packhash, epoch_id);
+
+    info!("=> pack {} written for epoch {} in {}", hex::encode(&packhash[..]),
+          epoch_id, duration_print(epoch_time_elapsed));
 }
 
 pub fn get_peer(blockchain: &str, cfg: &net::Config, native: bool) -> Peer {
