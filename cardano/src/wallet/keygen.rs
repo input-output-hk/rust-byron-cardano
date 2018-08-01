59722b85b14b53f320b679f45a4cc2b183d7e848
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-08-01 19:06:31 +0100

    spurious space

--- cardano/src/wallet/keygen.rs
+++ cardano/src/wallet/keygen.rs
@@ -7,7 +7,7 @@ use cryptoxide::pbkdf2::{pbkdf2};
 pub type Entropy = bip39::Entropy;
 
 /// Create a new seed from entropy and password
-/// 
+///
 /// The output size of pbkdf2 is associated with the size of the slice, allowing
 /// to generate a seed of the size required for various specific cryptographic object
 pub fn generate_seed(entropy: &Entropy, password: &[u8], output: &mut [u8]) {
