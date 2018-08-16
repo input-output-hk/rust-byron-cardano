33880ebd9fec99b6eff68d581f08b8b346b65f39
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-08-16 23:10:51 +0100

    add wallet sync's state update

--- cardano-cli/src/wallet/commands.rs
+++ cardano-cli/src/wallet/commands.rs
@@ -1,14 +1,16 @@
 use super::config::{encrypt_primary_key, Config, HDWalletModel};
 use super::{Wallet};
-use super::state;
+use super::state::{log, state, lookup, ptr, iter::TransactionIterator, utxo::UTxO};
+use super::error::{Error};
 
 use std::{path::PathBuf};
-use cardano::{hdwallet::{self, DerivationScheme}, wallet, bip::bip39};
+use cardano::{hdwallet::{self, DerivationScheme}, address::ExtendedAddr, wallet, bip::bip39, block::{BlockDate}};
 use rand::random;
 
 use utils::term::Term;
 
 use blockchain::{self, Blockchain};
+use serde;
 
 /// function to create a new wallet
 ///
@@ -147,45 +149,171 @@ pub fn sync( mut term: Term
     let blockchain = load_attached_blockchain(&mut term, root_dir, wallet.config.attached_blockchain.clone());
 
     // 2. prepare the wallet state
-    let initial_ptr = state::ptr::StatePtr::new_before_genesis(blockchain.config.genesis_prev);
+    let initial_ptr = ptr::StatePtr::new_before_genesis(blockchain.config.genesis.clone());
     match wallet.config.hdwallet_model {
         HDWalletModel::BIP44 => {
             let mut state = {
-                // TODO: add public key in the config file in the case of wallet
-                //       with bip44 scheme. So we don't need a password
-                let wallet = wallet.get_wallet_bip44(&[]).unwrap(); // TODO bad unwrap
-                let lookup_struct = state::lookup::sequentialindex::SequentialBip44Lookup::new(wallet);
-                state::state::State::new(initial_ptr, lookup_struct)
+                let lookup_struct = load_bip44_lookup_structure(&mut term, &wallet);
+                state::State::new(initial_ptr, lookup_struct)
             };
 
-            // 3. update the initial state with existing logs
-            let log_lock = wallet.log().unwrap(); // TODO check for Error::WalletLogAlreadyLocked(process_id)
-            state.update_with_logs(
-                state::log::LogReader::open(log_lock).unwrap() // BAD
-                    .into_iter().filter_map(|r| {
-                        match r {
-                            Err(err) => {
-                                panic!("{:?}", err)
-                            },
-                            Ok(v) => Some(v)
-                        }
-                    })
-            ).unwrap(); // BAD
+            update_wallet_state_with_logs(&wallet, &mut state);
 
-
+            update_wallet_state_with_utxos(&mut term, &wallet, &blockchain, &mut state);
         },
         HDWalletModel::RandomIndex2Levels => {
-            let mut _state = {
-                // TODO: ask user to fill in the password of the wallet
-                const PASSWORD : &'static [u8] = &[];
-                let wallet = wallet.get_wallet_rindex(PASSWORD).unwrap(); // TODO bad unwrap
-                let lookup_struct = state::lookup::randomindex::RandomIndexLookup::from(wallet);
-                state::state::State::new(initial_ptr, lookup_struct)
+            let mut state = {
+                let lookup_struct = load_randomindex_lookup_structure(&mut term, &wallet);
+                state::State::new(initial_ptr, lookup_struct)
             };
 
-            // TODO
+            update_wallet_state_with_logs(&wallet, &mut state);
+
+            update_wallet_state_with_utxos(&mut term, &wallet, &blockchain, &mut state);
         },
     };
+}
+
+fn update_wallet_state_with_utxos<LS>(term: &mut Term, wallet: &Wallet, blockchain: &Blockchain, state: &mut state::State<LS>)
+    where LS: lookup::AddressLookup<AddressInput = ExtendedAddr>
+        , for<'de> LS::AddressOutput : serde::Deserialize<'de> + serde::Serialize + Clone + ::std::fmt::Debug
+{
+    let blockchain_tip = blockchain.load_tip().0;
+
+    let from_ptr = state.ptr().clone();
+    let from = from_ptr.latest_known_hash;
+    let from_date = from_ptr.latest_addr.unwrap_or(BlockDate::Genesis(0));
+    let num_blocks = blockchain_tip.date - from_date;
+
+    term.info(&format!("syncing wallet from {} to {}\n", from_date, blockchain_tip.date)).unwrap();
+
+    let mut progress = term.progress_bar(num_blocks as u64);
+    progress.message("loading transactions... ");
+
+    let mut last_block_date = from_date;
+    for res in TransactionIterator::new(&mut progress, blockchain.iter_to_tip(from).unwrap() /* BAD */) {
+        let (ptr, txaux) = res.unwrap(); // BAD
+
+        if let Some(addr) = ptr.latest_addr {
+            if last_block_date.get_epochid() != addr.get_epochid() {
+
+                let log_lock = lock_wallet_log(&wallet);
+                let mut writer = log::LogWriter::open(log_lock).unwrap();
+                let log : log::Log<ExtendedAddr> = log::Log::Checkpoint(ptr.clone());
+                writer.append(&log).unwrap();
+            }
+
+            last_block_date = addr.clone();
+        }
+
+        {
+            let logs = state.forward_with_txins(txaux.tx.inputs.iter()).unwrap();
+            let log_lock = lock_wallet_log(&wallet);
+            let mut writer = log::LogWriter::open(log_lock).unwrap();
+            for log in logs { writer.append(&log).unwrap(); }
+        }
+
+        {
+            let txid = txaux.tx.id();
+            let logs = state.forward_with_utxos(
+                txaux.tx.outputs.into_iter().enumerate().map(|(idx, txout)| {
+                    UTxO {
+                        transaction_id: txid.clone(),
+                        index_in_transaction: idx as u32,
+                        blockchain_ptr: ptr.clone(),
+                        credited_address: txout.address,
+                        credited_value: txout.value
+                    }
+                })
+            ).unwrap();
+
+            let log_lock = lock_wallet_log(&wallet);
+            let mut writer = log::LogWriter::open(log_lock).unwrap();
+            for log in logs { writer.append(&log).unwrap(); }
+        }
+    }
+    progress.end();
+}
+
+fn update_wallet_state_with_logs<LS>(wallet: &Wallet, state: &mut state::State<LS>)
+    where LS: lookup::AddressLookup
+        , for<'de> LS::AddressOutput : serde::Deserialize<'de>
+{
+    let log_lock = lock_wallet_log(wallet);
+    state.update_with_logs(
+        log::LogReader::open(log_lock).unwrap() // BAD
+            .into_iter().filter_map(|r| {
+                match r {
+                    Err(err) => {
+                        panic!("{:?}", err)
+                    },
+                    Ok(v) => Some(v)
+                }
+            })
+    ).unwrap(); // BAD
+}
+
+fn load_bip44_lookup_structure(term: &mut Term, wallet: &Wallet) -> lookup::sequentialindex::SequentialBip44Lookup {
+    // TODO: to prevent from the need of the password, we can ask the user to create accounts ahead.
+    //       if we store the wallet's account public keys in the config file we may not need for the
+    //       password (and for the private key).
+    term.info("Enter the wallet password.\n").unwrap();
+    let password = term.password("wallet password: ").unwrap();
+
+    let wallet = match wallet.get_wallet_bip44(password.as_bytes()) {
+        Err(Error::CannotRetrievePrivateKeyInvalidPassword) => {
+            term.error("Invalid wallet spending password").unwrap();
+            ::std::process::exit(1);
+        },
+        Err(Error::CannotRetrievePrivateKey(err)) => {
+            term.error(&format!("Cannot retrieve the private key of the wallet: {}", err)).unwrap();
+            term.info("The encrypted wallet password is in an invalid format. You might need to delete this wallet and recover it.").unwrap();
+            ::std::process::exit(1);
+        },
+        Err(err) => {
+            term.error(IMPOSSIBLE_HAPPENED).unwrap();
+            panic!("failing with an unexpected error {:#?}", err);
+        },
+        Ok(wallet) => { wallet }
+    };
+    lookup::sequentialindex::SequentialBip44Lookup::new(wallet)
+}
+fn load_randomindex_lookup_structure(term: &mut Term, wallet: &Wallet) -> lookup::randomindex::RandomIndexLookup {
+    // in the case of the random index, we may not need the password if we have the public key
+    term.info("Enter the wallet password.\n").unwrap();
+    let password = term.password("wallet password: ").unwrap();
+
+    let wallet = match wallet.get_wallet_rindex(password.as_bytes()) {
+        Err(Error::CannotRetrievePrivateKeyInvalidPassword) => {
+            term.error("Invalid wallet spending password").unwrap();
+            ::std::process::exit(1);
+        },
+        Err(Error::CannotRetrievePrivateKey(err)) => {
+            term.error(&format!("Cannot retrieve the private key of the wallet: {}", err)).unwrap();
+            term.info("The encrypted wallet password is in an invalid format. You might need to delete this wallet and recover it.").unwrap();
+            ::std::process::exit(1);
+        },
+        Err(err) => {
+            term.error(IMPOSSIBLE_HAPPENED).unwrap();
+            panic!("failing with an unexpected error {:#?}", err);
+        },
+        Ok(wallet) => { wallet }
+    };
+    lookup::randomindex::RandomIndexLookup::from(wallet)
+}
+
+fn lock_wallet_log(wallet: &Wallet) -> log::LogLock {
+    match wallet.log() {
+        Err(Error::WalletLogAlreadyLocked(pid)) => {
+            error!("Wallet's LOG already locked by another process or thread ({})\n", pid);
+            ::std::process::exit(1);
+        },
+        Err(err) => {
+            error!("{}", IMPOSSIBLE_HAPPENED);
+            panic!("`lock_wallet_log' has failed with an unexpected error {:#?}", err);
+        },
+        Ok(lock) => { lock }
+    }
 }
 
 fn load_attached_blockchain(term: &mut Term, root_dir: PathBuf, name: Option<String>) -> Blockchain {
@@ -199,3 +327,9 @@ fn load_attached_blockchain(term: &mut Term, root_dir: PathBuf, name: Option<Str
         }
     }
 }
+
+const IMPOSSIBLE_HAPPENED : &'static str = "The impossible happened
+The process will panic with an error message, this is because something
+unexpected happened. Please report the error message with the panic
+error message to: https://github.com/input-output-hk/rust-cardano/issues
+";
