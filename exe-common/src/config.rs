7b63cee99a1225d8bcd3f2c4e7178a01d15ce6a6
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-08-21 14:27:50 +0100

    rename testnet to staging

--- exe-common/src/config.rs
+++ exe-common/src/config.rs
@@ -214,10 +214,10 @@ pub mod net {
             }
         }
 
-        pub fn testnet() -> Self {
+        pub fn staging() -> Self {
             let mut peers = Peers::new();
             peers.push("iohk-hosts".to_string(), Peer::native("relays.awstest.iohkdev.io:3000".to_string()));
-            peers.push("hermes".to_string(), Peer::http("http://hermes.dev.iohkdev.io/testnet".to_string()));
+            peers.push("hermes".to_string(), Peer::http("http://hermes.dev.iohkdev.io/staging".to_string()));
             Config {
                 genesis: HeaderHash::from_hex(&"B365F1BE6863B453F12B93E1810909B10C79A95EE44BF53414888513FE172C90").unwrap(),
                 genesis_prev: HeaderHash::from_hex(&"c6a004d3d178f600cd8caa10abbebe1549bef878f0665aea2903472d5abf7323").unwrap(),
