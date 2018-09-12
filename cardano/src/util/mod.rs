709d8dc16a7ea917e4baa8112a5ca2817a36eb41
Nicolas Di Prima <nicolas.diprima@iohk.io>
2018-09-12 09:43:48 +0100

    add Trait TryFromSlice

--- cardano/src/util/mod.rs
+++ cardano/src/util/mod.rs
@@ -2,3 +2,4 @@ pub mod securemem;
 pub mod hex;
 pub mod base58;
 pub mod bits;
+pub mod try_from_slice;
---
+++ cardano/src/util/try_from_slice.rs
@@ -0,0 +1,19 @@
+
+/// type conversion to work along side objects that implements `AsRef<[u8]>`
+///
+/// The idea is to facilitate serialization/deserialization implementation for
+/// generic types by providing a simple common interface between the objects.
+///
+/// This trait is there as a replacement to [`TryFrom`] which is not yet stable.
+///
+/// [`TryFrom`]: https://doc.rust-lang.org/std/convert/trait.TryFrom.html
+///
+pub trait TryFromSlice : Sized {
+    /// the error kind. We expect Display for now so we don't have to
+    /// implement all instances yet to all the Error kinds but this
+    /// will soon be extended to require the `Error` trait
+    type Error: ::std::fmt::Debug;
+
+    /// attempt to construct the object from the given slice
+    fn try_from_slice(slice: &[u8]) -> Result<Self, Self::Error>;
+}
