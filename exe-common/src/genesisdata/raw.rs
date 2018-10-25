200e57e155f1709ac6aedb2a9de64be12f6ef623
Vincent Hanquez <vincent.hanquez@iohk.io>
2018-10-25 19:02:43 +0100

    move raw structure into own module and add more block version data

---
+++ exe-common/src/genesisdata/raw.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+pub struct GenesisData {
+    pub avvmDistr: HashMap<String, String>,
+    pub nonAvvmBalances: HashMap<String, String>,
+    pub protocolConsts: ProtocolConsts,
+    pub blockVersionData: BlockVersionData,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+pub struct ProtocolConsts {
+    pub k: usize,
+    pub protocolMagic: u32,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+pub struct BlockVersionData {
+    pub heavyDelThd: String,
+    pub maxBlockSize: String,
+    pub maxHeaderSize: String,
+    pub maxProposalSize: String,
+    pub maxTxSize: String,
+    pub mpcThd: String,
+    pub scriptVersion: u32,
+    pub slotDuration: String,
+    pub softforkRule: SoftforkRule,
+    pub txFeePolicy: TxFeePolicy,
+    pub unlockStakeEpoch: String,
+    pub updateImplicit: String,
+    pub updateProposalThd: String,
+    pub updateVoteThd: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+pub struct TxFeePolicy {
+    pub summand: String,
+    pub multiplier: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+pub struct SoftforkRule {
+    pub initThd: String,
+    pub minThd: String,
+    pub thdDecrement: String,
+}
--- exe-common/src/parse_genesis_data.rs
+++ exe-common/src/parse_genesis_data.rs
@@ -1,42 +1,16 @@
-use std::collections::{HashMap, BTreeMap};
+use std::collections::{BTreeMap};
 use std::io::Read;
 use serde_json;
 use cardano::{config, fee, block, coin, redeem};
 use base64;
 
-#[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
-struct RawGenesisData {
-    avvmDistr: HashMap<String, String>,
-    nonAvvmBalances: HashMap<String, String>,
-    protocolConsts: ProtocolConsts,
-    blockVersionData: BlockVersionData,
-}
-
-#[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
-struct ProtocolConsts {
-    k: usize,
-    protocolMagic: u32,
-}
-
-#[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
-struct BlockVersionData {
-    txFeePolicy: TxFeePolicy,
-}
-
-#[derive(Deserialize, Debug)]
-struct TxFeePolicy {
-    summand: String,
-    multiplier: String,
-}
+use genesisdata::raw;
 
 pub fn parse_genesis_data<R: Read>(json: R) -> config::GenesisData { // FIXME: use Result
 
     let data_value: serde_json::Value = serde_json::from_reader(json).unwrap();
     let genesis_prev = block::HeaderHash::new(data_value.to_string().as_bytes());
-    let data: RawGenesisData = serde_json::from_value(data_value.clone()).unwrap();
+    let data: raw::GenesisData = serde_json::from_value(data_value.clone()).unwrap();
 
     let parse_fee_constant = |s: &str| {
         let n = s.parse::<u64>().unwrap();
