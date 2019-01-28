7ceb10929b12c44fc00f10ec74929af6f6c1ca9c
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2019-01-28 22:30:01 +0200

    network-ntt: Fix up server::{Error,ErrorKind}

--- network-ntt/src/server/error.rs
+++ network-ntt/src/server/error.rs
@@ -3,10 +3,11 @@ use std::{error, fmt};
 /// Represents errors that can be returned by the node client implementation.
 #[derive(Debug)]
 pub struct Error {
-    kind: String,
+    kind: ErrorKind,
     source: Box<dyn error::Error + Send + Sync>,
 }
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum ErrorKind {
     Handshake,
     Connect,
@@ -37,7 +38,7 @@ impl error::Error for Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self.kind {
-            ErrorKind::Handshake=> write!(f, "error during handshake"),
+            ErrorKind::Handshake => write!(f, "error during handshake"),
             ErrorKind::Connect => write!(f, "error during connection"),
         }
     }
