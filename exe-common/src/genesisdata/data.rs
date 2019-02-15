5ed5fdf58ace43aeec99f6f5af0d9203d719fb78
Eelco Dolstra <edolstra@gmail.com>
2019-02-15 15:35:53 +0100

    exe-common: Merge genesis_data and genesisdata

--- exe-common/src/genesisdata/data.rs
+++ exe-common/src/genesisdata/data.rs
@@ -7,21 +7,21 @@ pub fn get_genesis_data(genesis_prev: &HeaderHash) -> Result<&str, HeaderHash> {
             .unwrap()
     {
         Ok(include_str!(
-            "../genesis/5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb.json"
+            "../../genesis/5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb.json"
         ))
     } else if genesis_prev
         == &HeaderHash::from_str("b7f76950bc4866423538ab7764fc1c7020b24a5f717a5bee3109ff2796567214")
             .unwrap()
     {
         Ok(include_str!(
-            "../genesis/b7f76950bc4866423538ab7764fc1c7020b24a5f717a5bee3109ff2796567214.json"
+            "../../genesis/b7f76950bc4866423538ab7764fc1c7020b24a5f717a5bee3109ff2796567214.json"
         ))
     } else if genesis_prev
         == &HeaderHash::from_str("c6a004d3d178f600cd8caa10abbebe1549bef878f0665aea2903472d5abf7323")
             .unwrap()
     {
         Ok(include_str!(
-            "../genesis/c6a004d3d178f600cd8caa10abbebe1549bef878f0665aea2903472d5abf7323.json"
+            "../../genesis/c6a004d3d178f600cd8caa10abbebe1549bef878f0665aea2903472d5abf7323.json"
         ))
     } else {
         Err(genesis_prev.clone())
--- exe-common/src/genesisdata/mod.rs
+++ exe-common/src/genesisdata/mod.rs
@@ -1,3 +1,4 @@
 pub mod parse;
 pub mod print;
 pub mod raw;
+pub mod data;
