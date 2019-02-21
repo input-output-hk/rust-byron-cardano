jccf091/rust-cardano

protocol-tokio: Use helper traits to reduce bound boilerplate

protocol-tokio/src/protocol/codec/message.rs
@@ -10,7 +10,10 @@ use cbor_event::{
     se::{self, Serializer},
 };
 
-use super::super::nt;
+use super::super::{
+    nt,
+    traits::{ProtocolBlock, ProtocolTransactionId},
+};
 use super::NodeId;
 use chain_core::property;
 
@@ -76,7 +79,7 @@ impl de::Deserialize for MessageType {
 pub type KeepAlive = bool;
 
 #[derive(Clone, Debug)]
-pub enum Message<B: property::Block + property::HasHeader, Tx: property::TransactionId> {
+pub enum Message<B: ProtocolBlock, Tx: ProtocolTransactionId> {
     CreateLightWeightConnectionId(nt::LightWeightConnectionId),
     CloseConnection(nt::LightWeightConnectionId),
     CloseEndPoint(nt::LightWeightConnectionId),
@@ -105,16 +108,12 @@ pub enum Message<B: property::Block + property::HasHeader, Tx: property::Transac
     Subscribe(nt::LightWeightConnectionId, KeepAlive),
 }
 
-impl<B: property::Block + property::HasHeader, Tx: property::TransactionId> Message<B, Tx>
+impl<B, Tx> Message<B, Tx>
 where
-    <B as property::Block>::Id: cbor_event::Deserialize,
-    <B as property::Block>::Id: cbor_event::Serialize,
-    B: cbor_event::Deserialize,
-    B: cbor_event::Serialize,
-    B::Header: cbor_event::Deserialize,
-    B::Header: cbor_event::Serialize,
-    Tx: cbor_event::Serialize,
-    Tx: cbor_event::Deserialize,
+    B: ProtocolBlock,
+    <B as property::Block>::Id: se::Serialize + de::Deserialize,
+    <B as property::HasHeader>::Header: se::Serialize + de::Deserialize,
+    Tx: ProtocolTransactionId,
 {
     pub fn to_nt_event(self) -> nt::Event {
         use self::nt::{ControlHeader::*, Event::*};
@@ -208,7 +207,7 @@ where
     }
 }
 
-fn decode_node_ack_or_syn<B: property::Block + property::HasHeader, Tx: property::TransactionId>(
+fn decode_node_ack_or_syn<B: ProtocolBlock, Tx: ProtocolTransactionId>(
     lwcid: nt::LightWeightConnectionId,
     bytes: &Bytes,
 ) -> Option<Message<B, Tx>> {

protocol-tokio/src/protocol/inbound_stream.rs
@@ -1,5 +1,7 @@
 use super::{
-    nt, ConnectionState, KeepAlive, LightWeightConnectionState, Message, NodeId, Response,
+    nt,
+    traits::{ProtocolBlock, ProtocolTransactionId},
+    ConnectionState, KeepAlive, LightWeightConnectionState, Message, NodeId, Response,
 };
 use super::{BlockHeaders, GetBlockHeaders, GetBlocks};
 use bytes::Bytes;
@@ -71,18 +73,19 @@ pub enum Inbound<B: property::Block + property::HasHeader, Tx: property::Transac
     Data(nt::LightWeightConnectionId, Bytes),
 }
 
-pub struct InboundStream<T, B: property::Block, Tx: property::TransactionId> {
+pub struct InboundStream<T, B, Tx> {
     stream: SplitStream<nt::Connection<T>>,
     state: Arc<Mutex<ConnectionState>>,
     phantoms: PhantomData<(B, Tx)>,
 }
-impl<T: AsyncRead, B: property::Block + property::HasHeader, Tx: property::TransactionId> Stream
-    for InboundStream<T, B, Tx>
+
+impl<T, B, Tx> Stream for InboundStream<T, B, Tx>
 where
-    B: cbor_event::Deserialize + cbor_event::Serialize,
-    B::Id: cbor_event::Deserialize + cbor_event::Serialize,
-    B::Header: cbor_event::Deserialize + cbor_event::Serialize,
-    Tx: cbor_event::Deserialize + cbor_event::Serialize,
+    T: AsyncRead,
+    B: ProtocolBlock,
+    Tx: ProtocolTransactionId,
+    <B as property::Block>::Id: cbor_event::Serialize + cbor_event::Deserialize,
+    <B as property::HasHeader>::Header: cbor_event::Serialize + cbor_event::Deserialize,
 {
     type Item = Inbound<B, Tx>;
     type Error = InboundError;
@@ -97,19 +100,13 @@ where
         }
     }
 }
-impl<T, B: property::Block + property::HasHeader, Tx: property::TransactionId>
-    InboundStream<T, B, Tx>
+impl<T, B, Tx> InboundStream<T, B, Tx>
 where
-    <B as property::Block>::Id: std::marker::Sized,
-    B::Header: std::marker::Sized,
-    <B as property::Block>::Id: cbor_event::Deserialize,
-    <B as property::Block>::Id: cbor_event::Serialize,
-    B: cbor_event::Deserialize,
-    B: cbor_event::Serialize,
-    B::Header: cbor_event::Deserialize,
-    B::Header: cbor_event::Serialize,
-    Tx: cbor_event::Deserialize,
-    Tx: cbor_event::Serialize,
+    T: AsyncRead,
+    B: ProtocolBlock,
+    Tx: ProtocolTransactionId,
+    <B as property::Block>::Id: cbor_event::Serialize + cbor_event::Deserialize,
+    <B as property::HasHeader>::Header: cbor_event::Serialize + cbor_event::Deserialize,
 {
     pub fn new(stream: SplitStream<nt::Connection<T>>, state: Arc<Mutex<ConnectionState>>) -> Self {
         InboundStream {

protocol-tokio/src/protocol/mod.rs
@@ -3,6 +3,7 @@ mod codec;
 mod connecting;
 mod inbound_stream;
 mod outbound_sink;
+mod traits;
 
 use chain_core::property;
 
@@ -24,6 +25,8 @@ pub use self::codec::{
 pub use self::connecting::{Connecting, ConnectingError};
 pub use self::inbound_stream::{Inbound, InboundError, InboundStream};
 pub use self::outbound_sink::{Outbound, OutboundError, OutboundSink};
+pub use self::traits::*;
+
 use std::marker::PhantomData;
 
 /// the connection state, shared between the `ConnectionStream` and the `ConnectionSink`.
@@ -68,17 +71,19 @@ impl ConnectionState {
 ///
 /// Once established call `split` to get the inbound stream
 /// and the outbound sink and starts processing queries
-pub struct Connection<T, B: property::Block, Tx: property::TransactionId> {
+pub struct Connection<T, B, Tx> {
     connection: nt::Connection<T>,
     state: Arc<Mutex<ConnectionState>>,
     phantoms: PhantomData<(B, Tx)>,
 }
 
-impl<
-        T: AsyncRead + AsyncWrite,
-        B: property::Block + property::HasHeader,
-        Tx: property::TransactionId,
-    > Connection<T, B, Tx>
+impl<T, B, Tx> Connection<T, B, Tx>
+where
+    T: AsyncRead + AsyncWrite,
+    B: ProtocolBlock,
+    Tx: ProtocolTransactionId,
+    <B as property::Block>::Id: cbor_event::Serialize + cbor_event::Deserialize,
+    <B as property::HasHeader>::Header: cbor_event::Serialize + cbor_event::Deserialize,
 {
     fn new(connection: nt::Connection<T>) -> Self {
         Connection {
@@ -107,17 +112,7 @@ impl<
         Accepting::new(inner)
     }
 
-    pub fn split(self) -> (OutboundSink<T, B, Tx>, InboundStream<T, B, Tx>)
-    where
-        B::Header: cbor_event::Serialize,
-        B::Header: cbor_event::Deserialize,
-        <B as property::Block>::Id: cbor_event::Serialize,
-        <B as property::Block>::Id: cbor_event::Deserialize,
-        B: cbor_event::Serialize,
-        B: cbor_event::Deserialize,
-        Tx: cbor_event::Serialize,
-        Tx: cbor_event::Deserialize,
-    {
+    pub fn split(self) -> (OutboundSink<T, B, Tx>, InboundStream<T, B, Tx>) {
         let state = self.state;
         let (sink, stream) = self.connection.split();
 

input-output-hk/rust-byron-cardano

protocol-tokio/src/protocol/codec/message.rs
@@ -7,7 +7,10 @@ use cbor_event::{
     se::{self, Serializer},
 };
 
-use super::super::nt;
+use super::super::{
+    nt,
+    chain_bounds::{ProtocolBlock, ProtocolTransactionId},
+};
 use super::NodeId;
 use chain_core::property;
 
@@ -73,7 +76,7 @@ impl de::Deserialize for MessageType {
 pub type KeepAlive = bool;
 
 #[derive(Clone, Debug)]
-pub enum Message<B: property::Block + property::HasHeader, Tx: property::TransactionId> {
+pub enum Message<B: ProtocolBlock, Tx: ProtocolTransactionId> {
     CreateLightWeightConnectionId(nt::LightWeightConnectionId),
     CloseConnection(nt::LightWeightConnectionId),
     CloseEndPoint(nt::LightWeightConnectionId),
@@ -102,16 +105,12 @@ pub enum Message<B: property::Block + property::HasHeader, Tx: property::Transac
     Subscribe(nt::LightWeightConnectionId, KeepAlive),
 }
 
-impl<B: property::Block + property::HasHeader, Tx: property::TransactionId> Message<B, Tx>
+impl<B, Tx> Message<B, Tx>
 where
-    <B as property::Block>::Id: cbor_event::Deserialize,
-    <B as property::Block>::Id: cbor_event::Serialize,
-    B: cbor_event::Deserialize,
-    B: cbor_event::Serialize,
-    B::Header: cbor_event::Deserialize,
-    B::Header: cbor_event::Serialize,
-    Tx: cbor_event::Serialize,
-    Tx: cbor_event::Deserialize,
+    B: ProtocolBlock,
+    <B as property::Block>::Id: se::Serialize + de::Deserialize,
+    <B as property::HasHeader>::Header: se::Serialize + de::Deserialize,
+    Tx: ProtocolTransactionId,
 {
     pub fn to_nt_event(self) -> nt::Event {
         use self::nt::{ControlHeader::*, Event::*};
@@ -181,7 +180,7 @@ where
     }
 }
 
-fn decode_node_ack_or_syn<B: property::Block + property::HasHeader, Tx: property::TransactionId>(
+fn decode_node_ack_or_syn<B: ProtocolBlock, Tx: ProtocolTransactionId>(
     lwcid: nt::LightWeightConnectionId,
     bytes: &Bytes,
 ) -> Option<Message<B, Tx>> {

protocol-tokio/src/protocol/inbound_stream.rs
@@ -1,4 +1,5 @@
 use super::{
+    chain_bounds::{ProtocolBlock, ProtocolTransactionId},
     nt, ConnectionState, KeepAlive, LightWeightConnectionState, Message, NodeId, Response,
 };
 use super::{BlockHeaders, GetBlockHeaders, GetBlocks};
@@ -71,18 +72,19 @@ pub enum Inbound<B: property::Block + property::HasHeader, Tx: property::Transac
     Data(nt::LightWeightConnectionId, Bytes),
 }
 
-pub struct InboundStream<T, B: property::Block, Tx: property::TransactionId> {
+pub struct InboundStream<T, B, Tx> {
     stream: SplitStream<nt::Connection<T>>,
     state: Arc<Mutex<ConnectionState>>,
     phantoms: PhantomData<(B, Tx)>,
 }
-impl<T: AsyncRead, B: property::Block + property::HasHeader, Tx: property::TransactionId> Stream
-    for InboundStream<T, B, Tx>
+
+impl<T, B, Tx> Stream for InboundStream<T, B, Tx>
 where
-    B: cbor_event::Deserialize + cbor_event::Serialize,
-    B::Id: cbor_event::Deserialize + cbor_event::Serialize,
-    B::Header: cbor_event::Deserialize + cbor_event::Serialize,
-    Tx: cbor_event::Deserialize + cbor_event::Serialize,
+    T: AsyncRead,
+    B: ProtocolBlock,
+    Tx: ProtocolTransactionId,
+    <B as property::Block>::Id: cbor_event::Serialize + cbor_event::Deserialize,
+    <B as property::HasHeader>::Header: cbor_event::Serialize + cbor_event::Deserialize,
 {
     type Item = Inbound<B, Tx>;
     type Error = InboundError;
@@ -97,19 +99,13 @@ where
         }
     }
 }
-impl<T, B: property::Block + property::HasHeader, Tx: property::TransactionId>
-    InboundStream<T, B, Tx>
+impl<T, B, Tx> InboundStream<T, B, Tx>
 where
-    <B as property::Block>::Id: std::marker::Sized,
-    B::Header: std::marker::Sized,
-    <B as property::Block>::Id: cbor_event::Deserialize,
-    <B as property::Block>::Id: cbor_event::Serialize,
-    B: cbor_event::Deserialize,
-    B: cbor_event::Serialize,
-    B::Header: cbor_event::Deserialize,
-    B::Header: cbor_event::Serialize,
-    Tx: cbor_event::Deserialize,
-    Tx: cbor_event::Serialize,
+    T: AsyncRead,
+    B: ProtocolBlock,
+    Tx: ProtocolTransactionId,
+    <B as property::Block>::Id: cbor_event::Serialize + cbor_event::Deserialize,
+    <B as property::HasHeader>::Header: cbor_event::Serialize + cbor_event::Deserialize,
 {
     pub fn new(stream: SplitStream<nt::Connection<T>>, state: Arc<Mutex<ConnectionState>>) -> Self {
         InboundStream {

protocol-tokio/src/protocol/mod.rs
@@ -1,4 +1,5 @@
 mod accepting;
+mod chain_bounds;
 mod codec;
 mod connecting;
 mod inbound_stream;
@@ -17,6 +18,7 @@ use std::{
 use tokio_io::{AsyncRead, AsyncWrite};
 
 pub use self::accepting::{Accepting, AcceptingError};
+pub use self::chain_bounds::*;
 pub use self::codec::{
     BlockHeaders, GetBlockHeaders, GetBlocks, HandlerSpec, HandlerSpecs, Handshake, KeepAlive,
     Message, MessageType, NodeId, ProtocolMagic, Response,
@@ -24,6 +26,7 @@ pub use self::codec::{
 pub use self::connecting::{Connecting, ConnectingError};
 pub use self::inbound_stream::{Inbound, InboundError, InboundStream};
 pub use self::outbound_sink::{Outbound, OutboundError, OutboundSink};
+
 use std::marker::PhantomData;
 
 /// the connection state, shared between the `ConnectionStream` and the `ConnectionSink`.
@@ -68,17 +71,19 @@ impl ConnectionState {
 ///
 /// Once established call `split` to get the inbound stream
 /// and the outbound sink and starts processing queries
-pub struct Connection<T, B: property::Block, Tx: property::TransactionId> {
+pub struct Connection<T, B, Tx> {
     connection: nt::Connection<T>,
     state: Arc<Mutex<ConnectionState>>,
     phantoms: PhantomData<(B, Tx)>,
 }
 
-impl<
-        T: AsyncRead + AsyncWrite,
-        B: property::Block + property::HasHeader,
-        Tx: property::TransactionId,
-    > Connection<T, B, Tx>
+impl<T, B, Tx> Connection<T, B, Tx>
+where
+    T: AsyncRead + AsyncWrite,
+    B: ProtocolBlock,
+    Tx: ProtocolTransactionId,
+    <B as property::Block>::Id: cbor_event::Serialize + cbor_event::Deserialize,
+    <B as property::HasHeader>::Header: cbor_event::Serialize + cbor_event::Deserialize,
 {
     fn new(connection: nt::Connection<T>) -> Self {
         Connection {
@@ -107,17 +112,7 @@ impl<
         Accepting::new(inner)
     }
 
-    pub fn split(self) -> (OutboundSink<T, B, Tx>, InboundStream<T, B, Tx>)
-    where
-        B::Header: cbor_event::Serialize,
-        B::Header: cbor_event::Deserialize,
-        <B as property::Block>::Id: cbor_event::Serialize,
-        <B as property::Block>::Id: cbor_event::Deserialize,
-        B: cbor_event::Serialize,
-        B: cbor_event::Deserialize,
-        Tx: cbor_event::Serialize,
-        Tx: cbor_event::Deserialize,
-    {
+    pub fn split(self) -> (OutboundSink<T, B, Tx>, InboundStream<T, B, Tx>) {
         let state = self.state;
         let (sink, stream) = self.connection.split();
 