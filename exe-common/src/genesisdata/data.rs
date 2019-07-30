1f6563c6dfb1323c54268701e4df19b73d759383
KtorZ <matthias.benkort@gmail.com>
2019-07-30 12:13:28 +0200

    update genesis data to new testnet

--- exe-common/src/genesisdata/data.rs
+++ exe-common/src/genesisdata/data.rs
@@ -10,11 +10,11 @@ pub fn get_genesis_data(genesis_prev: &HeaderHash) -> Result<&str, HeaderHash> {
             "../../genesis/5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb.json"
         ))
     } else if genesis_prev
-        == &HeaderHash::from_str("b7f76950bc4866423538ab7764fc1c7020b24a5f717a5bee3109ff2796567214")
+        == &HeaderHash::from_str("96fceff972c2c06bd3bb5243c39215333be6d56aaf4823073dca31afe5038471")
             .unwrap()
     {
         Ok(include_str!(
-            "../../genesis/b7f76950bc4866423538ab7764fc1c7020b24a5f717a5bee3109ff2796567214.json"
+            "../../genesis/96fceff972c2c06bd3bb5243c39215333be6d56aaf4823073dca31afe5038471.json"
         ))
     } else if genesis_prev
         == &HeaderHash::from_str("c6a004d3d178f600cd8caa10abbebe1549bef878f0665aea2903472d5abf7323")
